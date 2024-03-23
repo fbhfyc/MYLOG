557d9dc5bbdde880c0bc465d97567e1b259f0ace
fbhfyc <fbhfyc@163.com>
2024-03-23 09:44:38 +0800

    0323

--- test/clogtest.c
+++ test/clogtest.c
@@ -6,13 +6,15 @@ int main(int argc, char** argv)
 {
     log_init("./log.config",argv[0]);
 
+    char* test = "Hello world";
+    
+    while ( 1 ) {
     LOGW(MODULE_A, "%s--%d", "\n", "Hello world", 1);
     LOGE(MODULE_B, "%s--%d", "\n", "Hello world", 2);
 
-    char* test = "Hello world";
-
     A_LOGI(test);
     A_LOGW("%s--%d", test, 4);
+    }
 
     log_drop();
 
